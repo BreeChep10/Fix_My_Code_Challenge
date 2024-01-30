0ada179c759e8bbadcda531e87e2c7fc22bd1565
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2024-01-30 22:09:54 +0300

    task 2

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,37 +1,25 @@
 #!/usr/bin/python3
-"""
-User class
-"""
 
+class Square():
 
-class User():
-    """ Documentation """
+    def __init__(self, width=0, height=0, **kwargs):
+        self.width = width
+        self.height = height
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
-    def __init__(self):
-        """ Documentation """
-        self.__email = None
+    def area_of_my_square(self):
+        """Area of the square"""
+        return self.width * self.width
 
-    @property
-    def email(self):
-        """ Documentation """
-        return self.__email
+    def perimeter_of_my_square(self):
+        return self.width * 4  # Perimeter for a square is 4 times the side length
 
-    @email.setter
-    def email(self, value):
-        """ Documentation """
-        if type(value) is not str:
-            raise TypeError("email must be a string")
-        self.__email = value
-
-    """@property
-    def email(self):
-        """ """Documentation""" """
-        return self.__email
-    """
-
+    def __str__(self):
+        return f"Square: {self.width} x {self.height}"
 
 if __name__ == "__main__":
-
-    u = User()
-    u.email = "john@snow.com"
-    print(u.email)
+    s = Square(width=12, height=12)
+    print(s)
+    print("Area:", s.area_of_my_square())
+    print("Perimeter:", s.perimeter_of_my_square())
